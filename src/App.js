876d7e735fb81815caa4ca4af55a338ee3d58573
Prashant1101-Jain <jainprashant718@gmail.com>
2022-01-20 17:37:09 +0530

    Created homepage and it style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <HomePage></HomePage>
     </div>
-  );
-}
+    );
+};
 
 export default App;
--- src/homepage.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HomePage = () =>{
-  <div className='direc'>
-  </div> 
-}
-
-
-export default HomePage;

929889e361d3ee742ccc54bf4ffc4da7e6eff381
Prashant1101-Jain <jainprashant718@gmail.com>
2022-01-21 22:49:16 +0530

    Added header with links and also style it and create rotuing and also learn about routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shoppage/shoppage.component";
+import Header from "./component/header/header.component";
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+    <Header/>
+      <Switch>
+        <Route exact='true' path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
-    );
-};
+  );
+}
 
 export default App;

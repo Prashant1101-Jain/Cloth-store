d157f6cf5b300af8edb496535fe4dff58a089685
Prashant1101-Jain <jainprashant718@gmail.com>
2022-01-22 00:51:05 +0530

    Learn about advance css class name and also created and added sign in sign up page and created sign in component and custom button and learn about forms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shoppage/shoppage.component";
 import Header from "./component/header/header.component";
+import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact='true' path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path = '/signin' component={SignInSignUp}/>
       </Switch>
     </div>
   );
